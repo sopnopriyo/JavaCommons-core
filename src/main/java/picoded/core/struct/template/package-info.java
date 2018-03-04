1800ec75fad8399f001768e5e8be306b6ad09ceb
Eugene Cheah <eugeneqin@gmail.com>
2018-03-04 12:21:43 +0800

    Added package info for picoded.core.struct.template

---
+++ src/main/java/picoded/core/struct/template/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * Collection of "UnsupportedDefault" implmentations of common data structures.
+ * Used to quickly create new structures without implementing the full required java support.
+ * 
+ * In event when not-fully-implemented commands are used, polyfills are used in place, for suboptimal performance,
+ * but consistant expected result.
+ */
+package picoded.core.struct.template;
