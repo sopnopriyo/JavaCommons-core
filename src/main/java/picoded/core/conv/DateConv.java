60ccc62c18c8d88b500daaaaa318677b015747a0
Shahin <shahin@uilicious.com>
2018-03-20 10:12:07 +0800

    Dropping old date functionalities and adding minimal and commonly required date conversion format

---
+++ src/main/java/picoded/core/conv/DateConv.java
@@ -0,0 +1,93 @@
+package picoded.core.conv;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import org.apache.commons.lang3.time.DateUtils;
+import picoded.core.exception.ExceptionMessage;
+
+/**
+ * Convenience class to convert between date types.
+ * [NOTE: This is not vetted for general usage]
+ *
+ *
+ * ---------------------------------------------------------------------------------
+ * Common Date Pattern                    |   Example Date
+ * ---------------------------------------------------------------------------------
+ * dd-MM-yy                                 31-01-12
+ * dd-MM-yyyy	                            31-01-2012
+ * MM-dd-yyyy	                            01-31-2012
+ * yyyy-MM-dd	                            2012-01-31
+ * yyyy-MM-dd HH:mm:ss	                    2012-01-31 23:59:59
+ * yyyy-MM-dd HH:mm:ss.SSS a	            2012-01-31 10:59:59.999 PM
+ * yyyy-MM-dd HH:mm:ss.SSSZ	                2012-01-31 23:59:59.999+0100
+ * EEEEE MMMMM yyyy HH:mm:ss.SSSZ	        Saturday November 2012 10:45:42.720+0100
+ * ----------------------------------------------------------------------------------
+ **/
+public class DateConv extends DateUtils {
+
+	/**
+	 * Invalid constructor (throws exception)
+	 **/
+	protected DateConv() {
+		throw new IllegalAccessError(ExceptionMessage.staticClassConstructor);
+	}
+
+    /**
+     * Convert a Date from String to java.util.Date Class
+     * Given a Date as a String and its pattern
+     * Then, this function will build the Java Date object out of that  DateString and pattern
+     * @param dateStr
+     * @param pattern
+     * @return
+     */
+    public static Date fromDateString(final String dateStr, final String pattern) {
+        try {
+            return DateUtils.parseDate(dateStr, new String[]{pattern});
+        } catch (ParseException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    /**
+     * Convert a java.util.Date Object to String
+     * Specify the Date format and provide java.util.Date Object in the parameter
+     * This function would constructs Date String according to the format given
+     *
+     *
+     * ```
+     * // Conversion to a date string, from milliseconds timestamp
+     * DateConv.toDateString( new Date(timestamp), "YYYY-MM-DD" );
+     * ```
+     *
+     * @param date
+     * @param outputPattern
+     * @return
+     */
+    public static String toDateString(final Date date, final  String outputPattern) {
+        SimpleDateFormat formatter = new SimpleDateFormat(outputPattern);
+        return  formatter.format(date);
+    }
+
+    /**
+     * Convert Unix timestamp to Date in String according to the output pattern
+     * @param timestamp
+     * @param outputPattern
+     * @return
+     */
+    public static String fromUnixTimestamp(final long timestamp, final String outputPattern) {
+        return toDateString(new Date((long)timestamp*1000), outputPattern);
+    }
+
+    /**
+     * Format a date String from one pattern to another
+     * @param dateStr
+     * @param inputPattern
+     * @param outputPattern
+     * @return
+     */
+    public static String reformatDate(String dateStr, String inputPattern, String outputPattern) {
+        return toDateString( fromDateString(dateStr, inputPattern), outputPattern);
+    }
+}
---
+++ src/test/java/picoded/core/conv/DateConv_test.java
@@ -0,0 +1,106 @@
+package picoded.core.conv;
+
+/**
+ * Test cases for DateTime Converter Apart from testing the happy cases ,
+ * This class would also test the exceptional cases as well
+ */
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+
+import static org.junit.Assert.*;
+
+public class DateConv_test {
+
+    @Before
+    public void setUp() {
+    }
+
+    @After
+    public void tearDown() {
+
+    }
+
+    /**
+     * Invalid constructor test
+     * Expected exception
+     *
+     * @throws Exception
+     */
+    @Test(expected = IllegalAccessError.class)
+    public void invalidConstructor() throws Exception {
+        new DateConv();
+
+    }
+
+    /**
+     * Test for happy cases to convert String to java.util.Date
+     */
+    @Test
+    public void fromDateStringHappyCase() {
+        String dateFormat = "dd-MMM-yyyy";
+        SimpleDateFormat formatter = new SimpleDateFormat(dateFormat);
+        String dateInString = "7-Jun-2013";
+        Date exceptedDate = null;
+        Date actualDate = null;
+        try {
+            actualDate = DateConv.fromDateString(dateInString, dateFormat);
+            exceptedDate = formatter.parse(dateInString);
+        } catch (ParseException parseException) {
+            assertEquals(null, parseException);
+        }
+        assertEquals(exceptedDate, actualDate);
+    }
+
+    /**
+     * Test for exceptional cases to convert Date from String to java.util.Date
+     */
+    @Test(expected = IllegalArgumentException.class)
+    public void fromDateStringExceptionCase() {
+        String dateFormat = "dd-MMM-yyfyy";
+        Date actualDate = DateConv.fromDateString("32-Jun-2013", dateFormat);
+    }
+
+    /**
+     * Test for happy cases to convert java.util.Date to String
+     */
+    @Test
+    public void toDateStringHappyCase() {
+        String pattern = "dd-MMM-yyyy";
+        SimpleDateFormat formatter = new SimpleDateFormat(pattern);
+        Date date = new Date();
+        String actualDateString = DateConv.toDateString(date, pattern);
+        assertEquals(actualDateString, formatter.format(date));
+        assertNotEquals(actualDateString, "dd-MMM-yysyy");
+    }
+
+    /**
+     * Test for converting unix timestamp to Date String
+     */
+    @Test
+    public void fromUnixTimestampTest(){
+        String pattern = "dd-MMM-yyyy";
+        long unixTimestamp = 1372339860;
+        String actualDate = DateConv.fromUnixTimestamp(unixTimestamp, pattern);
+        String expectedDate = "27-Jun-2013";
+        assertEquals(expectedDate, actualDate);
+    }
+
+    /**
+     * Test for formatting a date from one pattern to another
+     */
+    @Test
+    public void reformatCode(){
+        String inputPattern = "dd-MMM-yyyy";
+        String outputPattern = "dd-MM-yyyy";
+        String actualDate = DateConv.reformatDate("27-Jun-2013", inputPattern, outputPattern);
+        String expectedDate = "27-06-2013";
+        assertEquals(expectedDate, actualDate);
+    }
+}
