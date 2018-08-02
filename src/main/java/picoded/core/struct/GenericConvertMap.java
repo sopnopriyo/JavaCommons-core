4573deda6e0ce0c5c290324e6e50040a74605450
Eugene Cheah <eugeneqin@gmail.com>
2018-08-02 15:15:26 +0800

    Fix build issue

--- src/main/java/picoded/core/struct/GenericConvertMap.java
+++ src/main/java/picoded/core/struct/GenericConvertMap.java
@@ -5,6 +5,7 @@ import java.util.Map;
 import java.util.UUID;
 import java.util.function.BiFunction;
 import java.util.Set;
+import java.util.Collections;
 import java.util.HashSet;
 
 import picoded.core.conv.GenericConvert;
